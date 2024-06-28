2cb30ac8e1ed2da02144c141f5173830f217a39d
Rofaalvin <rofaalvin22@gmail.com>
2024-06-28 15:16:15 +0700

    ngugmjm

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
-<script>
+<script setup>
+import Sidebar from './components/SideBar.vue'
 import { computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -27,6 +28,10 @@ export default {
 
 <template>
   <div>
+    <div class = "app">
+      <Sidebar />
+      <router-view/>
+    </div>
     <nav v-if="showNavbar" class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
       <div class="container">
         <router-link :to="{ name: 'home' }" class="navbar-brand">HOME</router-link>
@@ -59,3 +64,41 @@ export default {
   </div>
 </template>
 
+<style lang="scss">
+:root{
+  --primary:#4ade80;
+  --grey:#64748b;
+  --dark:#1e293b;
+  --dark-alt:#334155;
+  --light:#f1f5f9;
+  --sidebar-width: 300px;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
+}
+
+body{
+  background: var(--light);
+}
+
+button{
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app{
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+  }
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <aside>
+    
+  </aside>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
